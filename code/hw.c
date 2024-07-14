d69bdc8876473f54ec10158c2957c1a664ebc4bd
Cai-Xu-iKUN <2032874035@qq.com>
2024-07-14 21:12:11 +0800

    conpleted Lesson5

---
+++ code/hw.c
@@ -0,0 +1,58 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // typedef关键字可以把一个类型自定义一个名字，举例：
+    typedef int zhengxin;
+    zhengxin test_zhengxin = 0;
+    printf("%d\n", test_zhengxin);
+
+    char tang[] = {'Linkin', 'Park', 'forever'};
+    printf("%c\n", tang);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+
+}
