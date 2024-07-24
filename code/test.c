30587bc509aa5d0960a60f218078b440a7ee64f8
Cai-Xu-iKUN <2032874035@qq.com>
2024-07-24 22:04:26 +0800

    complete Lesson9

---
+++ code/test.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+void hello(void)
+{
+    printf("Hello C");
+    printf("Hello LP");
+}
+int main(void)
+{
+    int hello = 1;
+    printf("Hello Worldsdada");
+
+    hello();
+
+
+
+     return 0;
+}
